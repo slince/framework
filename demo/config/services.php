d4d366a3ed20c66f8e6eb7a16b3f54d9380d49b0
slince <taosikai@sina.cn>
2015-08-24 20:34:39 +0800

    up

--- demo/config/services.php
+++ demo/config/services.php
@@ -6,6 +6,14 @@ use Slince\Router\RouterFactory;
 return [
     'router' => function(Container $container) {
          $context = RequestContext::create()->fromRequest($container->get('app')->getRequest());
-         return RouterFactory::create($context);
+         $router = RouterFactory::create($context);
+         $routes = $router->getRoutes();
+         $routeCreateCallback = include __DIR__ . '/routes.php';
+         call_user_func($routeCreateCallback, $routes);
+         return $router;
     },
+    'view' => [
+        'class' => 'Slince\\Router\\ServiceFactory',
+        'arguments'
+    ]
 ];
