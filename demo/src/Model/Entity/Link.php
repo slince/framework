744a31039091d9f5788c58fb84c6d0172c24c839
slince <taosikai@sina.cn>
2015-09-01 20:51:46 +0800

    up

---
+++ demo/src/Model/Entity/Link.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Link Entity.
+ */
+class Link extends Entity
+{
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        'name' => true,
+        'src' => true,
+        'href' => true,
+        'sort' => true,
+        'status' => true,
+        'create_time' => true,
+        'modify_time' => true
+    ];
+}
