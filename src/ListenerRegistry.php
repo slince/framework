910e122e297f5017aaed05e123f3b38f7e3baac5
slince <taosikai@sina.cn>
2015-09-02 18:08:42 +0800

    up

--- src/ListenerRegistry.php
+++ src/ListenerRegistry.php
@@ -7,14 +7,40 @@ namespace Slince\Application;
 
 class ListenerRegistry
 {
+    /**
+     * 
+     * Application
+     * 
+     * @var AbstractApplication
+     */
     protected $_application;
+    
+    protected $_loaded = [];
     
     function __construct(ApplicationInterface $application)
     {
         $this->_application = $application;
     }
     
-    function register()
+    function register($objectName)
+    {
+        $this->_application->getDispatcher()
+            ->addSubscriber($this->load($objectName));
+        return $this;
+    }
+    
+    function load($objectName)
+    {
+        if (isset($this->_loaded[$objectName])) {
+            return $this->_loaded[$objectName];
+        }
+        $class = $this->_resolveClassName($objectName);
+        $instance = new $class();
+        $this->_loaded[$objectName] = $instance;
+        return $instance;
+    }
+    
+    function _resolveClassName($objectName)
     {
         
     }
