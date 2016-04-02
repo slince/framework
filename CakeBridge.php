9d143e9c9819c7f6f1871650cb022ca6a8ff348f
slince <taosikai@yeah.net>
2016-04-02 23:23:05 +0800

    cakephp orm兼容处理工作

---
+++ CakeBridge.php
@@ -0,0 +1,27 @@
+<?php
+namespace Slince\Application;
+
+use Slince\Di\Container;
+use Cake\Datasource\ConnectionManager;
+use Cake\Cache\Cache;
+use Cake\Core\Configure;
+
+class CakeBridge extends Bridge
+{
+
+    protected $name = 'db';
+
+    function initialize(Container $container)
+    {
+        parent::initialize($container);
+        list($datasources, $cache) = $this->getConfigs();
+        // 设置数据库
+        foreach ($datasources as $name => $config) {
+            ConnectionManager::config($name, $config);
+        }
+        // 设置缓存
+        foreach ($cache as $name => $config) {
+            Cache::config($name, $config);
+        }
+    }
+}
